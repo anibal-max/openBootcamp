3e223a442c461b27529ddb74f3ffd24d200a843e
Anibal <anibal.m.hillier@gmail.com>
2023-01-15 16:49:48 -0300

    Ejercicio Spring Data OK

---
+++ ejSpringData/src/main/java/com/example/ejSpringData/EjSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjSpringDataApplication.class, args);
+	}
+
+}
---
+++ ejSpringData/src/test/java/com/example/ejSpringData/EjSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
